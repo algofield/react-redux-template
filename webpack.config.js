62703a6ef37b4fa385701c05231171ac814ed5e7
cj dagostino <cjd.dagostino@gmail.com>
2023-02-08 12:50:58 -0500

    #3 added env config to frontend. Added http2 tokens

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,51 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 
-module.exports = {
-  mode: 'production',
-  entry: {
-    client: path.join(__dirname, 'src', 'client', 'index.js'),
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name]-bundle.js',
-  },
-  devServer: {
-    port: 8080,
-  },
-  plugins : [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'client', 'templates', 'index.ejs'),
-      filename: 'index.html',
-      inject: 'body',
-      // minify: true,
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'css-loader',
-          'style-loader',
-        ],
-      }
-    ]
-  }
+module.exports = () => {
+  
+  const env = dotenv.config().parsed;
+  const envKeys = Object.keys(env).reduce((acc, next) => {
+    acc[`process.env.${next}`] = JSON.stringify(env[next]);
+    return acc;
+  }, {})
+  return {
+    mode: process.env.NODE_ENV,
+    entry: {
+      client: path.join(__dirname, 'src', 'client', 'index.js'),
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: '[name]-bundle.js',
+    },
+    devServer: {
+      port: 8080,
+    },
+    plugins : [
+      new webpack.DefinePlugin(envKeys),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'src', 'client', 'templates', 'index.ejs'),
+        filename: 'index.html',
+        inject: 'body',
+        // minify: true,
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'css-loader',
+            'style-loader',
+          ],
+        }
+      ]
+    }
+  };
 };
