9319309ad8367eac705389c5a40b9ffed3c7a8e6
cj dagostino <cjd.dagostino@gmail.com>
2023-02-08 12:28:15 -0500

    #2 changed html template rendering in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,23 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: path.join(__dirname, 'src', 'client', 'index.js'),
+  mode: 'production',
+  entry: {
+    client: path.join(__dirname, 'src', 'client', 'index.js'),
+  },
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+    filename: '[name]-bundle.js',
   },
   devServer: {
     port: 8080,
   },
   plugins : [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'client', 'templates', 'index.html'),
+      template: path.join(__dirname, 'src', 'client', 'templates', 'index.ejs'),
+      filename: 'index.html',
+      inject: 'body',
+      // minify: true,
     }),
   ],
   module: {
